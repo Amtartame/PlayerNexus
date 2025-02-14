2a07273b28aa432e56055f8eff836921cd33aebc
JanjeVuk <janjevuk@outlook.be>
2025-02-14 16:08:33 +0100

    Make Connection field final in MysqlPersistentStorage
        The `connection` field is now declared as `final` to ensure it is initialized only once and cannot be reassigned. This change improves immutability and helps prevent potential bugs related to accidental reassignment.

--- src/main/java/akia/net/playerNexus/storage/MysqlPersistentStorage.java
+++ src/main/java/akia/net/playerNexus/storage/MysqlPersistentStorage.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.*;
 
 public class MysqlPersistentStorage implements PersistentStorage {
-    private Connection connection;
+    private final Connection connection;
     private final List<String> modelKeys;
     private final String tableName = "player_data";
 
